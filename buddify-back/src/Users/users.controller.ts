b1e9f44b7bdb3a9a4a07e4e0e8331035a73ef985
Velentin Garro <nelsonvgarrodadan@gmail.com>
2024-11-11 09:09:54 -0300

    endpoint register

--- buddify-back/src/Users/users.controller.ts
+++ buddify-back/src/Users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/createUserDto.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly userService:UsersService){}
+    @Post()
+    register(@Body() newUser:CreateUserDto):Promise<{message:string;}>{
+        return this.userService.register(newUser);
+    }
+}
