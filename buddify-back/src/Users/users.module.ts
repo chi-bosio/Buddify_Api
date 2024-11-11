f9cdd8b8304cc181fac3f56c66c47230d5612f78
Velentin Garro <nelsonvgarrodadan@gmail.com>
2024-11-11 09:09:19 -0300

    injection dependency Credentials

--- buddify-back/src/Users/users.module.ts
+++ buddify-back/src/Users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './users.entity';
 import { UsersRepository } from './users.repository';
+import { Credentials } from './credentials/credentials.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [TypeOrmModule.forFeature([Users,Credentials])],
   controllers: [UsersController],
   providers: [UsersService,UsersRepository]
 })
