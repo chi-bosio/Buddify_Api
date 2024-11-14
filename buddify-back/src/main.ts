662d667f8ddd72e4ea444c7a164e7b02658d0707
Velentin Garro <nelsonvgarrodadan@gmail.com>
2024-11-14 12:49:45 -0300

    feact/cors

--- buddify-back/src/main.ts
+++ buddify-back/src/main.ts
@@ -5,9 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
-  app.use(loggerGblobal)
-  await app.listen(process.env.PORT ?? 3000);
-  console.log('server listening on http://localhost:3000')
+  app.use(loggerGblobal);
+  app.enableCors()
+
+  const port = process.env.PORT ?? 3001;
+  await app.listen(port);
+  console.log(`Server listening on http://localhost:${port}`);
 }
+
 bootstrap();
