99f37e09f59c1d1ee8bd48ee2ee03a321300d202
Lisandro85 <lisandrobedotti@hotmail.com>
2024-11-22 14:46:41 +0100

    implementacion de stripe

--- buddify-back/src/app.module.ts
+++ buddify-back/src/app.module.ts
@@ -5,13 +5,16 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ActivityModule } from './modules/activities/Activity.module';
 import { CategoryModule } from './modules/categories/category.module';
+import { StripeModule } from './modules/stripe/stripe.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig), 
+  imports: [
+    TypeOrmModule.forRoot(ormConfig),
     UsersModule,
     AuthModule,
     ActivityModule,
     CategoryModule,
+    StripeModule,
   ],
   controllers: [],
   providers: [],
