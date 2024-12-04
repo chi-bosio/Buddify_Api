eaf91a85541608252aaaf339b5c76d5d7b2f86fe
Velentin Garro <nelsonvgarrodadan@gmail.com>
2024-12-04 12:40:26 -0300

    Update orm-config.ts

--- src/config/orm-config.ts
+++ src/config/orm-config.ts
@@ -13,6 +13,6 @@ const ormConfig: TypeOrmModuleOptions = {
   entities: ['dist/**/*.entity{.ts,.js}'],
   autoLoadEntities: true,
   synchronize: true,
-  dropSchema: false,
+  dropSchema: true,
 };
 export default ormConfig;
